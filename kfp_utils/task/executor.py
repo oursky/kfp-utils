6347ced85e1e2b6cb5ab4255928c74135f33f365
Jason Wu <jasonkit321@gmail.com>
2022-03-21 10:58:56 +0800

    Reduce number of thread for downloading files in parallel
    As downloading too many large file in parallel will lead to OOM.

--- kfp_utils/task/executor.py
+++ kfp_utils/task/executor.py
@@ -3,6 +3,6 @@ from concurrent import futures
 
 
 class BoundedThreadPoolExecutor(futures.ThreadPoolExecutor):
-    def __init__(self, maxsize=50, *args, **kwargs):
+    def __init__(self, maxsize=4, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._work_queue = queue.Queue(maxsize=maxsize)
